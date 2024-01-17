e5aa5db3f9901c7fa8b6f361ab45739d26d02dd6
purvasshah <purvas3010@gmail.com>
2024-01-18 00:26:14 +0530

    Added JS to add task

--- script.js
+++ script.js
@@ -2,11 +2,33 @@ const tdValue = document.getElementById("tdtext"),
 listItems = document.getElementById("list-items"),
 addUpdateClick = document.getElementById("AddUpdateClick");
 
+tdValue.addEventListener("keypress", function (e)
+{
+    if(e.key === "Enter")
+    {
+        addUpdateClick.click();
+    }
+});
+
 function createTodoData()
 {
-    console.log(tdValue.value);
-    if(tdValue.value)
+    if(tdValue.value === "")
     {
-        
+        alert("Please enter a task");
+        tdValue.focus();
     }
+    else
+    {
+        let li = document.createElement("li");
+        const tdItems = `<div ondlbclick="CompleteTodoItem(this)">${tdValue.value}</div> <div><img class="edit td-controlsOne" src="pencil.png" alt="edit" /><img class="delete td-controls" src="bin.png" alt="delete" /></div>`;
+
+        li.innerHTML = tdItems;
+        listItems.appendChild(li);
+        tdValue.value = "";
+    }
+}
+
+function CompleteTodoItem(e)
+{
+    console.log(e.parentElement);
 }

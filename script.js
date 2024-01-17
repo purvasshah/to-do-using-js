e5e305e75ab81625b36889885c21122ba93186a8
purvasshah <purvas3010@gmail.com>
2024-01-17 22:49:39 +0530

    Added javascript declaration

---
+++ script.js
@@ -0,0 +1,12 @@
+const tdValue = document.getElementById("tdtext"),
+listItems = document.getElementById("list-items"),
+addUpdateClick = document.getElementById("AddUpdateClick");
+
+function createTodoData()
+{
+    console.log(tdValue.value);
+    if(tdValue.value)
+    {
+        
+    }
+}
